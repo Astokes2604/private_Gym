4fb45a031e5a822aefce21c3d0f7a7b924c4bee4
Astokes2604 <129238930+Astokes2604@users.noreply.github.com>
2025-01-17 16:38:31 -0700

    started work on the front end
    the pages that anyone can see whether they are logged in or not

--- front-end/src/components/Main/Home.js
+++ front-end/src/components/Main/Home.js
@@ -1,21 +1,38 @@
-import React from 'react'
-import Catalog from './Catalog';
-import AboutUs from './AboutUs';
-import'./Main.css';
+import React from 'react';
+import { Link } from "react-router-dom";
+import './Main.css';
 
-const Home = () => {
+const Home = ({ isAuthenticated, handleLogout }) => {
     return (
         <div>
-            <h1 id='stokes'>Stokes Gym</h1>
-            <nav>
-                <AboutUs />
-                <Catalog />
-            </nav>
             <div>
-                <h3 id='potential'>Unlock Your Potential!</h3>
+                <h3 id="potential">Unlock Your Potential!</h3>
+                <p id="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+            </div>
+
+            <div id="info-section">
+                <section className="info-section">
+                    <h4>About Us</h4>
+                    <p>Learn more about Stokes Gym, our mission, values, and what we stand for.</p>
+                </section>
+                <hr />
+                <section className="info-section">
+                    <h4>Catalog</h4>
+                    <p>Explore our catalog of gym memberships, personal training services, and products.</p>
+                </section>
+                <hr />
+                <section className="info-section">
+                    <h4>Login</h4>
+                    <p>Access your account to manage your memberships, schedule, and more.</p>
+                </section>
+                <hr />
+                <section className="info-section">
+                    <h4>Register</h4>
+                    <p>Create a new account to join the Stokes Gym community.</p>
+                </section>
             </div>
         </div>
-    )
+    );
 }
 
 export default Home;
